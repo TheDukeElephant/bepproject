071f4e8a85f3397172d7c4c250aea9d0c8611198
Sep Ursone <sepursone@gmail.com>
2024-11-20 00:42:14 +0100

    debug time

--- run.py
+++ run.py
@@ -1,17 +1,19 @@
-from app import create_app, socketio
-from app.background import background_co2_read
-from app.database import init_db
+#from app import create_app, socketio
+from app import create_app
+#from app.background import background_co2_read
+#from app.database import init_db
 
 # Initialize the app
 app = create_app()
 
 # Initialize the database
-init_db()
+#init_db()
 
 # Start background tasks
-socketio.start_background_task(target=background_co2_read)
+#socketio.start_background_task(target=background_co2_read)
 
 if __name__ == "__main__":
     print("Starting Flask application...")
     #socketio.run(app, host="172.20.10.11", port=5000)
-    socketio.run(app, host="localhost", port=5000)
+    #socketio.run(app, host="localhost", port=5000)
+    app.run(app, host="localhost", port=5000)

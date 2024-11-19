3ae5516353e15926d6b30aff7c2ebc058ae6fdde
Sep Ursone <sepursone@gmail.com>
2024-11-20 00:48:50 +0100

    test socketio

--- run.py
+++ run.py
@@ -1,13 +1,13 @@
 from app import create_app, socketio
 #from app import create_app
 from app.background import background_co2_read
-from app.database import init_db
+#from app.database import init_db
 
 # Initialize the app
 app = create_app()
 
 # Initialize the database
-init_db()
+#init_db()
 
 # Start background tasks
 socketio.start_background_task(target=background_co2_read)

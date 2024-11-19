261d44d7959dde2c2621a17f5e4d79fd907a1f74
Sep Ursone <sepursone@gmail.com>
2024-11-19 23:52:37 +0100

    deprecated flask code

--- app/background.py
+++ app/background.py
@@ -22,7 +22,7 @@ def background_co2_read():
     while True:
         if ser is None:
             print("Serial port is unavailable. Emitting placeholder value.")
-            socketio.emit('update_data', {'co2': '?'}, broadcast=True)
+            socketio.emit('update_data', {'co2': '?'}, to='/')
         else:
             try:
                 ser.write(b'Z\r\n')
@@ -30,10 +30,11 @@ def background_co2_read():
                 response = ser.read_until().decode('utf-8').strip()
                 if response.startswith('Z') and len(response) > 1:
                     co2_value = int(response[1:])
-                    socketio.emit('update_data', {'co2': co2_value}, broadcast=True)
+                    socketio.emit('update_data', {'co2': co2_value}, to='/')
                 else:
-                    socketio.emit('update_data', {'co2': '?'}, broadcast=True)
+                    socketio.emit('update_data', {'co2': '?'}, to='/')
             except Exception as e:
                 print(f"Error reading from serial port: {e}")
-                socketio.emit('update_data', {'co2': '?'}, broadcast=True)
+                socketio.emit('update_data', {'co2': '?'}, to='/')
         time.sleep(1)
+

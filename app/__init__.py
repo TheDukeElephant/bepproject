4bbd88c162fb4c54744ed17f313fddb8b5237579
Sep Ursone <sepursone@gmail.com>
2024-11-20 00:40:57 +0100

    edit init file to make sure flask is ok

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,18 @@
 from flask import Flask
-from flask_login import LoginManager
 from flask_socketio import SocketIO
+from flask_login import LoginManager
 
-# Extensions
-login_manager = LoginManager()
 socketio = SocketIO()
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'super_secret_key'  # Replace with environment variable in production
+    app.secret_key = "super_secret_key"  # Replace with a secure key
 
     # Initialize extensions
-    login_manager.init_app(app)
     socketio.init_app(app)
-    
+    login_manager.init_app(app)
+
     # Register blueprints
     from .routes import main_blueprint
     from .auth import auth_blueprint

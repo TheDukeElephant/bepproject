068a8a844f5a3045d6f2fee38f4bc5425d5ecc76
Sep Ursone <sepursone@gmail.com>
2024-11-20 01:11:05 +0100

    new login redirect

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required, current_user
 
 main_blueprint = Blueprint('main', __name__)
 
 @main_blueprint.route('/')
-@login_required
 def index():
-    return render_template('/index.html')
+    # Redirect to login if the user is not authenticated
+    if not current_user.is_authenticated:
+        return redirect(url_for('auth.login'))
+    return render_template('index.html')

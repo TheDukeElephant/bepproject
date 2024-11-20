d4ef509761729b2802c1b576c9d21cf9316f2d30
Sep Ursone <sepursone@gmail.com>
2024-11-20 01:03:24 +0100

    excluding co2 read

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,4 @@ main_blueprint = Blueprint('main', __name__)
 @main_blueprint.route('/')
 @login_required
 def index():
-    return render_template('index.html')
+    return render_template('/index.html')

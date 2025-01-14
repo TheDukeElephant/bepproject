5be4f459da43927c5933dd82c24c46650215242d
Sep Ursone <sepursone@gmail.com>
2025-01-14 13:43:11 +0100

    neww updatew

--- run.py
+++ run.py
@@ -2,24 +2,27 @@ from app import create_app, socketio
 from app.background import background_sensor_read
 from app.database import init_db
 from wifi_monitor import start_wifi_monitor
-from app import socketio, app  # Adjust this import based on your Flask app structure
 
-# Initialize the app
+# Initialize the Flask application
 app = create_app()
 
 # Initialize the database
 init_db()
 
-
 if __name__ == "__main__":
-    
     print("Starting background tasks...")
-    # Start background tasks
+    
+    # Start Wi-Fi monitoring in a background task
     start_wifi_monitor()
+    
+    # Start the sensor reading task in the background
     socketio.start_background_task(target=background_sensor_read)
-    print("background tasks started")
+    
+    print("Background tasks started")
     print("Starting Flask application...")
+    
     try:
+        # Run the Flask-SocketIO server
         socketio.run(app, host="0.0.0.0", port=5000, debug=True, use_reloader=False)
     except Exception as e:
         print(f"Error running Flask-SocketIO: {e}")

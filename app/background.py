d2ec5b3d26c3ed19b18b0fcdd440029442c91796
Sep Ursone <sepursone@gmail.com>
2025-03-05 14:23:51 +0100

    meer nederlandse comments

--- app/background.py
+++ app/background.py
@@ -19,27 +19,25 @@ import csv
 import subprocess
 import RPi.GPIO as GPIO
 
-# Define the output file path
+# output file kiezen naam
 OUTPUT_FILE = "sensor_data.csv"
 
 
 threads_started = False
 
-#interval how often the control function is run (in seconds)
 CONTROL_INTERVAL_TEMP = 10
 
 CONTROL_INTERVAL_CO2 = 30
 TIME_CO2_SOLENOID_ON = 0.1
-# Default fallback values which will all be filtered out in js
-FALLBACK_CO2 = 40  # Example fallback CO2 level in ppm
-FALLBACK_O2 = 23  # Example fallback O2 level in %
-FALLBACK_TEMPERATURE = 960  # Example fallback temperature in °C
-FALLBACK_HUMIDITY = 101.0  # Example fallback humidity in %
+FALLBACK_CO2 = 40 
+FALLBACK_O2 = 23 
+FALLBACK_TEMPERATURE = 960 
+FALLBACK_HUMIDITY = 101.0  
 
-# Buffer to store recent data for reconnections
-data_buffer = deque(maxlen=10)  # Store up to the last 10 updates
 
-# Initialize I2C bus and OLED display
+data_buffer = deque(maxlen=10) 
+
+
 i2c = None
 oled = None
 try:
@@ -50,30 +48,20 @@ except Exception as e:
     print(f"Error initializing OLED display: {e}")
     oled = None
 
-# Initialize SPI bus for MAX31865 sensors
 spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
 cs_pins = [board.D5, board.D6, board.D13, board.D19, board.D26]
 sensors = [adafruit_max31865.MAX31865(spi, digitalio.DigitalInOut(cs), rtd_nominal=100.0, ref_resistor=430.0) for cs in cs_pins]
 
-# Initialize AM2302 (DHT22) humidity sensor
+
 DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4  # GPIO pin number
+DHT_PIN = 4  
 
-# Initialize oxygen sensor (MIX8410)
-#oxygen_sensor_pin = digitalio.DigitalInOut(board.D14)
-#oxygen_sensor_pin.direction = digitalio.Direction.INPUT
-
-# Initialize CO2 sensors
-#co2_sensor_pins = [digitalio.DigitalInOut(pin) for pin in [board.D15, board.D8]]
-#for co2_sensor_pin in co2_sensor_pins:
-#    co2_sensor_pin.direction = digitalio.Direction.INPUT
 
 
 
 def get_ip_address():
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     try:
-        # doesn't even have to be reachable
         s.connect(("8.8.8.8", 80))
         ip_address = s.getsockname()[0]
     except Exception as e:
@@ -116,7 +104,6 @@ def read_humidity():
 
 def read_oxygen():
     try:
-        # Read the analog value from the MIX8410 sensor
         oxygen_value = oxygen_sensor_pin.value
         return oxygen_value
     except Exception as e:
@@ -124,19 +111,19 @@ def read_oxygen():
         return FALLBACK_O2
     
 def standby_oled():
-    """Display 'Standby...' on the OLED screen."""
+
     if oled:
         try:
-            # Clear the screen
+            # scherm reset
             oled.fill(0)
             oled.show()
             
-            # Create an image to display 'Standby...'
+            
             image = Image.new('1', (oled.width, oled.height))
             draw = ImageDraw.Draw(image)
             font = ImageFont.load_default()
             
-            # Display 'Standby...' on the OLED screen
+            
             draw.text((0, 30), "Standby...", font=font, fill=255)
             oled.image(image)
             oled.show()
@@ -145,22 +132,22 @@ def standby_oled():
             print(f"Error displaying 'Standby...' on OLED: {e}")
 
 
-#When turning off the python script, display will say: Standby...
+#standby functie voor als je het script sluit
 atexit.register(standby_oled)
 
-# Write a header row to the output file if it doesn't already exist
+
 def initialize_output_file():
     try:
         with open(OUTPUT_FILE, 'a') as file:
             writer = csv.writer(file)
-            # Write header only if the file is empty
+            
             if file.tell() == 0:
                 writer.writerow(['timestamp', 'co2', 'o2', 'temperature_1', 'temperature_2',
                                  'temperature_3', 'temperature_4', 'average_temperature', 'humidity'])
     except Exception as e:
         print(f"Error initializing output file: {e}")
 
-# Save sensor data to the output file
+# data opslaan belangrijk voor de grafieken van de incubator
 def save_to_file(sensor_data):
     try:
         with open(OUTPUT_FILE, 'a') as file:
@@ -175,45 +162,44 @@ def save_to_file(sensor_data):
     except Exception as e:
         print(f"Error saving to output file: {e}")
 
-# Initialize the output file at the start of the script
+# output file nodig voor het opslaan van de waardes
 initialize_output_file()
 
 def control_temperature(average_temperature):
     """Turn the temperature on or off based on temperature."""
-    temperature_pin = 27  # Get the humidifier GPIO pin
+    temperature_pin = 27  
 
     if average_temperature < 36.9:
-        GPIO.output(temperature_pin, GPIO.LOW)  # Turn ON the humidifier (LOW for ON)
+        GPIO.output(temperature_pin, GPIO.LOW)  
         print("Temperature heating turned ON")
     elif average_temperature > 37.1:
-        GPIO.output(temperature_pin, GPIO.HIGH)  # Turn OFF the humidifier (HIGH for OFF)
+        GPIO.output(temperature_pin, GPIO.HIGH) 
         print("Temperature heating turned OFF")
 
 def control_co2(co2_value):
     """Turn the co2 on or off based on co2."""
-    temperature_pin = 4  # Get the co2 solenoid GPIO pin
+    temperature_pin = 4  
 
     if co2_value > 0.01 and co2_value < 5:
-        GPIO.output(temperature_pin, GPIO.LOW)  # Turn ON the humidifier (LOW for ON)
+        GPIO.output(temperature_pin, GPIO.LOW)  
         print("CO2 solenoid turned ON")
         time.sleep(TIME_CO2_SOLENOID_ON)
-        GPIO.output(temperature_pin, GPIO.HIGH)  # Turn OFF the humidifier (HIGH for OFF)
+        GPIO.output(temperature_pin, GPIO.HIGH) 
         print("CO2 solenoid turned OFF")
     elif co2_value > 5:
-        GPIO.output(temperature_pin, GPIO.HIGH)  # Turn OFF the humidifier (HIGH for OFF)
+        GPIO.output(temperature_pin, GPIO.HIGH) 
         print("CO2 solenoid turned OFF")
 
-# Control threads
+
 def temperature_control_thread():
     while True:
         try:
-            # Dynamically calculate the average temperature
+            
             temperatures = [read_temperature(sensor) for sensor in sensors]
             average_temperature = round((temperatures[2] + temperatures[3]) / 2, 2)
             
-            # Control temperature based on the average
             control_temperature(average_temperature)
-            time.sleep(CONTROL_INTERVAL_TEMP)  # Run at specified intervals
+            time.sleep(CONTROL_INTERVAL_TEMP) 
         except Exception as e:
             print(f"Error in temperature control thread: {e}")
 
@@ -221,29 +207,27 @@ def temperature_control_thread():
 def co2_control_thread():
     while True:
         try:
-            # Dynamically fetch the CO2 value
             ser = initialize_serial()
             if ser is not None:
                 co2_value = get_co2_value_from_serial(ser)
             else:
                 co2_value = FALLBACK_CO2
 
-            # Control CO2 based on the fetched value
             control_co2(co2_value)
-            time.sleep(CONTROL_INTERVAL_CO2)  # Run at specified intervals
+            time.sleep(CONTROL_INTERVAL_CO2)  
         except Exception as e:
             print(f"Error in CO2 control thread: {e}")
 
 
 def background_sensor_read():
-    ser = initialize_serial()  # Initialize CO₂ sensor via UART
-    last_temperature_control_time_temp = time.time()  # Initialize the last control time
-    last_temperature_control_time_co2 = time.time()  # Initialize the last control time
-    
+    #zet uart aan
+    ser = initialize_serial()  
+    last_temperature_control_time_temp = time.time()  
+    last_temperature_control_time_co2 = time.time()  
     global threads_started
     
     if not threads_started:
-        # Start control threads only once
+        # multithreading want is beter
         threading.Thread(target=temperature_control_thread, daemon=True).start()
         threading.Thread(target=co2_control_thread, daemon=True).start()
         threads_started = True
@@ -251,28 +235,28 @@ def background_sensor_read():
 
     while True:
         try:
-            # Reinitialize the serial connection if it becomes invalid
+            # serial down? nog een keer
             if ser is None or not ser.is_open:
                 print("Reinitializing serial connection...")
                 ser = initialize_serial()
 
-            # Initialize fallback values
+            # fallbacks
             co2_value = FALLBACK_CO2
-            o2_value = FALLBACK_O2  # Use a constant fallback since it's not dynamically read
+            o2_value = FALLBACK_O2  
             temperatures = [read_temperature(sensor) for sensor in sensors]
             humidity = read_humidity()
             
-            # Calculate average temperature
+            # gemid temp
             average_temperature = round((temperatures[2] + temperatures[3]) / 2, 2)
 
-            # Control temperature every 10 seconds
+            # elke 10 sec controlleer de temperatuur	
             current_time = time.time()
             if current_time - last_temperature_control_time_temp >= CONTROL_INTERVAL_TEMP:
                 control_temperature(average_temperature)
                 last_temperature_control_time_temp = current_time
 
             
-            # CO₂ Sensor Reading
+            # co2 lezen met serial port, doe voorzichtig want sensor werkt niet goed, niet te vaak aansturen.
             if ser is not None:
                 co2_value = get_co2_value_from_serial(ser)
 
@@ -281,7 +265,7 @@ def background_sensor_read():
                 control_co2(co2_value)
                 last_temperature_control_time_co2 = current_time
 
-            # Prepare sensor data to emit
+            # data voordat emit word in array
             sensor_data = {
                 'timestamp': int(time.time()),
                 'co2': co2_value,
@@ -290,23 +274,23 @@ def background_sensor_read():
                 'humidity': round(humidity, 2)
             }
 
-            # Save the data to the file
+            # data saving naar csv bestand
             save_to_file(sensor_data)
 
-            # Store data in buffer
+            # data buffer omdat wifi niet goed werkt
             data_buffer.append(sensor_data)
 
-            # Emit the data to connected clients
+            # data moet naar alle clienten
             socketio.emit('update_dashboard', sensor_data, to=None)
             print("Emitting data successfully.")
 
-            # Check for "Not connected" conditions
+            # als niet in range, zorg dat er not connected staat voor het gemak van de gebruiker
             display_temp = "Not connected" if sensor_data['temperatures'][5] > 950 else f"{sensor_data['temperatures'][5]} C"
             display_humidity = "Not connected" if sensor_data['humidity'] > 100 else f"{sensor_data['humidity']} %"
             display_o2 = "Not connected" if sensor_data['o2'] > 21 else f"{sensor_data['o2']} %"
             display_co2 = "Not connected" if sensor_data['co2'] > 21 else f"{sensor_data['co2']} %"
 
-            # Display data on the OLED if available
+            # oled scherm aanzetten met de goede library en alvast functies aansturen die ip adres een wifi ssid krijgen
             if oled:
                 ip_address = get_ip_address()
                 wifi_ssid = get_wifi_ssid()
@@ -314,7 +298,7 @@ def background_sensor_read():
                 draw = ImageDraw.Draw(image)
                 font = ImageFont.load_default()
                 
-                # Display information on the OLED screen
+                # oled schermpje weergave
                 draw.text((0, 0), f"SSID: {wifi_ssid}", font=font, fill=255)
                 draw.text((0, 10), f"IP: {ip_address}", font=font, fill=255)
                 draw.text((0, 20), f"Temp: {display_temp}", font=font, fill=255)
@@ -327,7 +311,7 @@ def background_sensor_read():
 
         except Exception as e:
             print(f"Error reading sensors: {e}")
-            # Fallback data on failure
+            # data moet altijd een fallback hebben voor als een sensor breekt.
             fallback_data = {
                 'timestamp': int(time.time()),
                 'co2': round(FALLBACK_CO2 / 10000, 2),
@@ -362,7 +346,7 @@ def process_co2_response(response):
     return FALLBACK_CO2
 
 temperatures = [read_temperature(sensor) for sensor in sensors]
-# Utility functions
+# gemiddelde temperaturen berekenen want handig voor het project.
 def calculate_average_temperature(temperatures):
     average_temperature = round((temperatures[2] + temperatures[3]) / 2, 2)
     
